b8c9730f687170adf854571616d1f32139d599d2
kaaviya98 <kaaviya@testpress.in>
2022-12-05 18:25:19 +0530

    Add_password_changing_support_to_the_account

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,4 +5,14 @@ from . import views
 
 urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
 ]
