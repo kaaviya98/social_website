cef2dc7ed3c6ab7fe8fd87d0b81f758507849a4d
kaaviya98 <kaaviya@testpress.in>
2022-12-06 08:14:56 +0530

    Add_password_resetting_support

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,18 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
     path("dashboard/", views.dashboard, name="dashboard"),
-    path(
-        "password_change/",
-        auth_views.PasswordChangeView.as_view(),
-        name="password_change",
-    ),
-    path(
-        "password_change/done/",
-        auth_views.PasswordChangeDoneView.as_view(),
-        name="password_change_done",
-    ),
+    path("", include("django.contrib.auth.urls")),
 ]
