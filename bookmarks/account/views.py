1878affc2205e92a246ada16a2309ceba562318b
Kaaviya Elango <113660479+kaaviya98@users.noreply.github.com>
2022-12-07 18:29:19 +0530

    Add user registration (#5)
    * Add_user_registration
    
    * Refactor: Views

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
+from django.contrib import messages
 
 
 @login_required
 def dashboard(request):
     return render(request, "account/dashboard.html", {"section": "dashboard"})
+
+
+def validate_the_registeration_form(request):
+    user_form = UserRegistrationForm(data=request.POST)
+    if user_form.is_valid():
+        new_user = user_form.save(commit=False)
+        new_user.set_password(user_form.cleaned_data["password"])
+        new_user.save()
+        messages.success(
+            request,
+            "Registration successful you can login now to your account.",
+        )
+    else:
+        messages.error(
+            request, "Unsuccessful registration. Invalid information."
+        )
+    return render(
+        request, "account/register.html", {"form": UserRegistrationForm()}
+    )
+
+
+def register(request):
+    if request.method == "POST":
+        validate_the_registeration_form(request)
+    return render(
+        request, "account/register.html", {"form": UserRegistrationForm()}
+    )
