43f52eaf45d4f66adc8767324db7d42a1b5f0ee4
kaaviya98 <kaaviya@testpress.in>
2022-12-11 13:39:55 +0530

    Display activity steam in dashboard

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -10,11 +10,25 @@ from django.views.decorators.http import require_POST
 from bookmarks.common.decorators import ajax_required
 from .models import Contact
 from actions.utils import create_action
+from actions.models import Action
 
 
 @login_required
 def dashboard(request):
-    return render(request, "account/dashboard.html", {"section": "dashboard"})
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list("id", flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+
+    actions = actions.select_related("user", "user__profile").prefetch_related(
+        "target"
+    )[:10]
+
+    return render(
+        request,
+        "account/dashboard.html",
+        {"section": "dashboard", "actions": actions},
+    )
 
 
 def validate_the_registeration_form(request):
