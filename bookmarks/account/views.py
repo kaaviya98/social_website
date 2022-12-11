a7e2bade9ac009c9c2333d039f338f94a942139c
kaaviya98 <kaaviya@testpress.in>
2022-12-11 12:45:45 +0530

    Add follow system support to the account app

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -5,6 +5,10 @@ from django.contrib import messages
 from .models import Profile
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from django.http import JsonResponse, response
+from django.views.decorators.http import require_POST
+from bookmarks.common.decorators import ajax_required
+from .models import Contact
 
 
 @login_required
@@ -83,3 +87,28 @@ def user_detail(request, username):
         "account/user/detail.html",
         {"section": "people", "user": user},
     )
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == "follow":
+                Contact.objects.get_or_create(
+                    user_from=request.user, user_to=user
+                )
+            else:
+                Contact.objects.filter(
+                    user_from=request.user, user_to=user
+                ).delete()
+            return JsonResponse({"status": "ok"})
+
+        except User.DoesNotExist:
+            return JsonResponse({"status": "error"})
+
+    return JsonResponse({"status": "error"})
--- bookmarks/test/account/test_urls.py
+++ bookmarks/test/account/test_urls.py
@@ -1,6 +1,13 @@
 from django.test import TestCase
 from django.urls import resolve, reverse
-from account.views import dashboard, register, edit, user_list, user_detail
+from account.views import (
+    dashboard,
+    register,
+    edit,
+    user_list,
+    user_detail,
+    user_follow,
+)
 from test.account.test_model_mixin import ModelMixinTestCase
 
 
@@ -32,4 +39,10 @@ class TestUrls(ModelMixinTestCase, TestCase):
         self.assertEqual(
             (resolve(reverse("user_detail", args=["john"])).func),
             user_detail,
+        )
+
+    def test_account_user_follow_url_is_resolved(self):
+        self.assertEqual(
+            (resolve(reverse("user_follow")).func),
+            user_follow,
         )
