0be1637208304a6babdcc0886b165e19da87b8a9
Kaaviya Elango <113660479+kaaviya98@users.noreply.github.com>
2022-12-14 10:09:58 +0530

    Create detail view for image app (#13)
    * Create detail view for image app
    
    * Refactor with base.html
    
    * Refactor:Reformatting

--- bookmarks/test/images/test_views.py
+++ bookmarks/test/images/test_views.py
@@ -23,3 +23,39 @@ class ImageCreateView(ModelMixinTestCase, TestCase):
             },
         )
         self.assertTrue(Image.objects.filter(user=self.user).exists())
+
+    def test_template_used_with_detail_view(self):
+        self.client.login(username="john", password="johnpassword")
+
+        Image.objects.create(
+            user=self.user,
+            title="test-image",
+            slug="test-image",
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Berlin_Opera_UdL_asv2018-05.jpg/800px-Berlin_Opera_UdL_asv2018-05.jpg",
+        )
+        response = self.client.get(
+            reverse("images:detail", args=[1, "test-image"])
+        )
+        self.assertTemplateUsed(response, "images/image/detail.html")
+
+    def test_image_from_database_exists_in_detail_view(self):
+        self.client.login(username="john", password="johnpassword")
+
+        self.image = Image.objects.create(
+            user=self.user,
+            title="test-image",
+            slug="test-image",
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Berlin_Opera_UdL_asv2018-05.jpg/800px-Berlin_Opera_UdL_asv2018-05.jpg",
+        )
+        response = self.client.get(
+            reverse("images:detail", args=[1, "test-image"])
+        )
+        self.assertEqual(response.context.get("image"), self.image)
+
+    def test_detail_view_returns_404_for_no_image(self):
+        self.client.login(username="john", password="johnpassword")
+
+        response = self.client.get(
+            reverse("images:detail", args=[1, "test-image"])
+        )
+        self.assertEqual(response.status_code, 404)
