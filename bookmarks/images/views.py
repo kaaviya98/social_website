b54f4788eaaaadf890638b15473eb9d53a1673e3
Kaaviya Elango <113660479+kaaviya98@users.noreply.github.com>
2022-12-14 10:14:51 +0530

    Add support to like an image in the detail page (#15)
    * Add support to like an image in the detail page
    
    * Refactor
    
    * Refactor:Add testcases
    
    * Refactor:Views
    
    * Refactor

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
@@ -33,3 +35,23 @@ def image_detail(request, id, slug):
         "images/image/detail.html",
         {"section": "images", "image": image},
     )
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == "like":
+                image.users_like.add(request.user)
+                response = JsonResponse({"status": "ok"})
+            elif image.users_like.count() == 1:
+                image.users_like.remove(request.user)
+                response = JsonResponse({"status": "ok"})
+            return response
+        except Exception:
+            pass
+    return JsonResponse({"status": "error"})
--- bookmarks/test/images/test_views.py
+++ bookmarks/test/images/test_views.py
@@ -59,3 +59,61 @@ class ImageCreateView(ModelMixinTestCase, TestCase):
             reverse("images:detail", args=[1, "test-image"])
         )
         self.assertEqual(response.status_code, 404)
+
+    def test_image_like_succeeds_with_valid_image(self):
+        self.client.login(username="john", password="johnpassword")
+
+        self.image = Image.objects.create(
+            user=self.user,
+            title="test",
+            slug="test",
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Berlin_Opera_UdL_asv2018-05.jpg/800px-Berlin_Opera_UdL_asv2018-05.jpg",
+        )
+        response = self.client.post(
+            reverse("images:like"),
+            {"id": 1, "action": "like"},
+        )
+
+        self.assertEqual(response.content.decode(), '{"status": "ok"}')
+
+    def test_image_unlike_succeeds_with_valid_image(self):
+        self.client.login(username="john", password="johnpassword")
+
+        self.image = Image.objects.create(
+            user=self.user,
+            title="tests",
+            slug="tests",
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Berlin_Opera_UdL_asv2018-05.jpg/800px-Berlin_Opera_UdL_asv2018-05.jpg",
+        )
+        self.client.post(
+            reverse("images:like"),
+            {"id": 1, "action": "like"},
+        )
+
+        response = self.client.post(
+            reverse("images:like"),
+            {"id": 1, "action": "unlike"},
+        )
+
+        self.assertEqual(response.content.decode(), '{"status": "ok"}')
+
+    def test_image_like_fails_without_image_id_and_action(self):
+        self.client.login(username="john", password="johnpassword")
+
+        self.image = Image.objects.create(
+            user=self.user,
+            title="test",
+            slug="test",
+            image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Berlin_Opera_UdL_asv2018-05.jpg/800px-Berlin_Opera_UdL_asv2018-05.jpg",
+        )
+        response = self.client.post(
+            reverse("images:like"), {"id": "None", "action": "None"}
+        )
+        self.assertEqual(response.content.decode(), '{"status": "error"}')
+
+    def test_image_like_directly_called_returns_status_code_200(self):
+        self.client.login(username="john", password="johnpassword")
+        response = self.client.post(
+            reverse("images:like"),
+        )
+        self.assertEqual(response.status_code, 200)
