b9f52c0a5c5f8d1433b0dfc01b527a705caf6819
kaaviya98 <kaaviya@testpress.in>
2022-12-20 10:25:39 +0530

    Add test cases

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -55,8 +55,8 @@ class ImageListView(ListView):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     total_views = redis_client.incr(f"image:{image.id}:views")
-    total_views = r.incr(f"image:{image.id}:views")
-    r.zincrby("image_ranking", 1, image.id)
+    total_views = redis_client.incr(f"image:{image.id}:views")
+    redis_client.zincrby("image_ranking", 1, image.id)
     return render(
         request,
         "images/image/detail.html",
@@ -87,7 +87,7 @@ def image_like(request):
 
 @login_required
 def image_ranking(request):
-    image_ranking = r.zrange("image_ranking", 0, -1, desc=True)[:10]
+    image_ranking = redis_client.zrange("image_ranking", 0, -1, desc=True)[:10]
     image_ranking_ids = [int(id) for id in image_ranking]
     most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
     most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
